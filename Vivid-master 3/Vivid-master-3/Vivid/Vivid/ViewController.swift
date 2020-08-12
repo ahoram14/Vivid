d16729f4e9c99bf0fe7f8c67f5ecbe53c3495801
ahoram14 <69254557+ahoram14@users.noreply.github.com>
2020-08-12 14:32:10 -0400

    homescreen to profile page segues

---
+++ Vivid-master 3/Vivid-master-3/Vivid/Vivid/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Vivid
+//
+//  Created by Amira Horam on 8/10/20.
+//  Copyright © 2020 Amira Horam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unWindToOne (_ sender: UIStoryboardSegue){
+        
+    }
+}
+
